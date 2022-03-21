cecfd95733bfe5ebd96df03ae3d42de282075a00
Ivan Alieksieiev <89075086+JustImprove@users.noreply.github.com>
2022-03-21 12:44:48 +0200

    Add files via upload

---
+++ Multiplication.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+char DetermineDigit(int Digit) 
+{
+    string Buffer;
+    for (size_t i = 1; i < Digit + 1; i++)
+    {
+        Buffer += to_string((i*i));
+    }
+    return Buffer[Digit - 1];
+} 
+
+int main() 
+{
+    int Digit;
+    cin >> Digit;
+    if (Digit > 0 || Digit < 3200000)
+    {        
+        cout << "Position - \t" <<  DetermineDigit(Digit) << endl;
+    }
+    else 
+    {
+        return -1;
+    }
+}
